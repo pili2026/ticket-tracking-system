1f2e42889a3b8a5a885791c0045f9478bc886617
pili2026 <b9813114@gmail.com>
2020-10-15 21:46:31 +0800

    add authority page

---
+++ src/main/java/com/ticket/tracking/service/CustomLoginUserDetailsService.java
@@ -0,0 +1,85 @@
+package com.ticket.tracking.service;
+
+import com.ticket.tracking.entity.Role;
+import com.ticket.tracking.entity.User;
+import com.ticket.tracking.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class CustomLoginUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User findUserById(String id) {
+        return userRepository.findUserById(id);
+    }
+
+    public List<User> findUsers() {
+        return userRepository.findAll();
+    }
+
+    public List<User> findRDUsers() {
+        return userRepository.findUserByUserRole("RD");
+    }
+
+
+    public void saveUser(User user) {
+        user.setEmail(user.getEmail());
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setEnabled(true);
+        user.setFullName(user.getFullName());
+        Role userRole = roleRepository.findByRole(user.getUserRole());
+        user.setRoles(new HashSet<>(Collections.singletonList(userRole)));
+        userRepository.save(user);
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email);
+        if (user != null) {
+            List<GrantedAuthority> authorities = getUserAuthority(user.getRoles());
+            return buildUserForAuthentication(user, authorities);
+        } else {
+            throw new UsernameNotFoundException("username not found");
+        }
+    }
+
+    private List<GrantedAuthority> getUserAuthority(Set<Role> userRoles) {
+        Set<GrantedAuthority> roles = new HashSet<>();
+        userRoles.forEach((role) -> {
+            roles.add(new SimpleGrantedAuthority(role.getRole()));
+        });
+
+        return new ArrayList<>(roles);
+    }
+
+    private UserDetails buildUserForAuthentication(User user, List<GrantedAuthority> authorities) {
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/ticket/tracking/service/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.ticket.tracking.service;
+
+import com.ticket.tracking.entity.Role;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends MongoRepository<Role, String> {
+
+    Role findByRole(String role);
+
+}
