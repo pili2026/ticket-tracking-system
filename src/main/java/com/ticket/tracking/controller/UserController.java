4df5ef7baf60ff0ad5e2729f287ad0e44db58f1d
pili2026 <b9813114@gmail.com>
2020-10-16 00:35:52 +0800

    need fix create user

--- src/main/java/com/ticket/tracking/controller/UserController.java
+++ src/main/java/com/ticket/tracking/controller/UserController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,7 +25,7 @@ public class UserController {
         return ResponseEntity.ok(users);
     }
 
-    @PutMapping(value = "/add_user")
+    @PostMapping(value = "/add_user")
     public ResponseEntity<User> createUser(User user, BindingResult bindingResult) {
         User userExists = customLoginUserDetailsService.findUserByEmail(user.getEmail());
         if (userExists != null) {
